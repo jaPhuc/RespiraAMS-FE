ff965743077c7134b70994e8ce92149e8c91139f
GPhuc <phuctlg.ce190872@gmail.com>
2026-05-25 15:29:26 +0700

    Fix directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono, Inter } from "next/font/google"
 
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import "@/src/app/globals.css"
+import { ThemeProvider } from "@/src/components/theme-provider"
+import { cn } from "@/src/lib/utils";
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'})
 
